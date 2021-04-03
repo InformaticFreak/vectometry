2961a4908ea255a6546fa7c77085a15eb61f836d
Fabian <fabianbartl.2003@outlook.de>
2021-04-03 17:27:06 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = "1.0.0"
+VERSION = "1.0.1"
 DESCRIPTION = "Simple use of vectors and points with vector operations"
 LONG_DESCRIPTION = "Defines a vector and point object with vector operations as functions and with integration of built-in operators for bette usability."
 
@@ -16,14 +16,14 @@ setup(
     name="vector-geometry",
     version=VERSION,
     author="InformaticFreak",
-    author_email="<yt.informaticfreak@gmail.com@gmail.com>",
+    author_email="<yt.informaticfreak@gmail.com>",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
-    install_requires=["math"],
-    keywords=["python", "vector", "point", "geometry", "analytic geometry", "vector oberations", "vectors"],
-	url="https://github.com/InformaticFreak/vector-geometry"
+    install_requires=[],
+    keywords=["python", "vector", "point", "geometry", "analytic geometry", "vector operations", "vectors"],
+	url="https://github.com/InformaticFreak/vector-geometry",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Intended Audience :: Developers",
