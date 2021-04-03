a5f8d9c89017605b512c858b34e38b0f7ea98283
Fabian <fabianbartl.2003@outlook.de>
2021-04-03 14:40:00 +0200

    Update __init__.py

--- src/vectors/__init__.py
+++ src/vectors/__init__.py
@@ -147,7 +147,7 @@ class Point:
 	def __neg__(self): return neg(self)
 	def __pos__(self): return self
 	
-	# Comparison operations as location relationships between vectors
+	# Comparison operations
 	def __eq__(self, a): return self.x() == a.x() and self.y() == a.y() and self.z() == a.z()
 	def __ne__(self, a): return not self.__eq__(a)
 	
