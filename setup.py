398dba536876b822dd3a3a2feb817baa44c17f6f
Fabian <fabianbartl.2003@outlook.de>
2021-04-03 19:11:37 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,23 +7,28 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = "1.0.1"
+VERSION = "0.0.1"
 DESCRIPTION = "Simple use of vectors and points with vector operations"
-LONG_DESCRIPTION = "Defines a vector and point object with vector operations as functions and with integration of built-in operators for bette usability."
+LONG_DESCRIPTION = "Defines a vector and point object with vector operations as functions and with integration of built-in operators for better usability."
 
 # Setting up
 setup(
-    name="vector-geometry",
+    name="vector-operations",
     version=VERSION,
     author="InformaticFreak",
     author_email="<yt.informaticfreak@gmail.com>",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description=LONG_DESCRIPTION,
-    packages=find_packages(),
     install_requires=[],
-    keywords=["python", "vector", "point", "geometry", "analytic geometry", "vector operations", "vectors"],
-	url="https://github.com/InformaticFreak/vector-geometry",
+    keywords=["python", "vector", "point", "geometry", "analytic geometry", "vector operations", "vector geometry", "vectors"],
+	url="https://github.com/InformaticFreak/vector-operations",
+	project_urls={
+		"Bug Tracker": "https://github.com/InformaticFreak/vector-operations/issues",
+	},
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.6",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Intended Audience :: Developers",
